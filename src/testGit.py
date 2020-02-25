e3adf239a0af86b479ed9b5d44ed71920795c1a0
王丛 <105859705@qq.com>
2020-02-25 21:12:45 +0800

    Revert "111"
    This reverts commit 4861bc5a

---
+++ src/testGit.py
@@ -0,0 +1,20 @@
+# this is a test for Git. Let's see the answer
+
+
+a = 123
+print(a)
+
+
+a = 123
+print(a)
+
+
+a = 123
+print(a)
+
+
+a = 123
+print(a)
+
+if __name__ == "__main__":
+    print(555)
