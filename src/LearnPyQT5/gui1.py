1d52c905112f59bc3914df6fa528f2ed903b85c0
王丛 <105859705@qq.com>
2020-02-25 21:11:16 +0800

    333

--- src/LearnPyQT5/gui1.py
+++ src/LearnPyQT5/gui1.py
@@ -5,11 +5,9 @@ from PyQt5.QtWidgets import *
 app = QApplication(sys.argv)
 
 win = QWidget()
-win.resize(450,150)
-win.move(500,300)
+win.resize(450, 150)
+win.move(500, 300)
 win.setWindowTitle("好好学习，天天向上")
 win.show()
-
-
 
 sys.exit(app.exec_())
